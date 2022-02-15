31d84f91a7d27a38115f56eeff499dfd4ac1533e
S637\adamo <votrubova.ada@gmail.com>
2022-02-15 21:04:57 +0100

    work in progres zacatek

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 // tady je místo pro náš program
+
+console.log("ahoooj :-)");
+
+let nadpis = document.querySelector("h1");
+nadpis.classList.add("zluty");
+nadpis.classList.remove("zluty")
+
+let ctverecek = document.querySelector("div.zluty");
+ctverecek.classList.add("ctverecek");
+
+let odstavec =document.querySelector ("ul")
+odstavec.classList.add("red");
+odstavec.style.fontSize ="30px";
+
+
+
+
+
+
+**/ Domácí úkol 1: Tlačítko “červené po kliknutí” bude odstavci přidávat/odebírat třídu, která bude barvu písma v odstavci měnit na červenou.
