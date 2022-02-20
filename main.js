5bc28fcf9e2948c638591bc28fca1094ca8c52a2
S637\adamo <votrubova.ada@gmail.com>
2022-02-20 15:24:27 +0100

    opravene chyby

--- main.js
+++ main.js
@@ -13,17 +13,17 @@ ctverecek.classList.add("ctverecek");
 let odstavec= document.querySelector("p")
 
 function ztucni(){
-    console log("test ztucni");
+    console.log("test ztucni");
     odstavec.style.fontWeight="bold";
 }
  
 function ztenci(){
-    console log("test ztenci")
+    console.log("test ztenci")
     odstavec.style.fontWeight="normal";
 }
 
 function prikliknuti(){
-    odstavec.classList.toggle("red");
+    odstavec.classList.toggle(".red");
    }
 
 let pocitadlo= document.querySelector(".odstavec")
@@ -37,4 +37,9 @@ function zvetsi(){
 Úkol na lekci: Písmo v odstavci po najetí myši ztuční.
 Domácí úkol 1: Tlačítko “červené po kliknutí” bude odstavci přidávat/odebírat třídu, která bude barvu písma v odstavci měnit na červenou.
 Domácí úkol 2: přidej tlačítko “větší po kliknutí”, které písmo zvětší o jeden pixel oproti jeho aktuální velikost.
+Domácí úkol 3: Dopracuj ovládání audio souboru
+    pauznutí zvukové stopy
+    nastavení zvuku na 0, 0.5 a 1
+    vrácení se na začátek
+
 */
