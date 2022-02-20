2bfd853c34dc056365c691b8a691603486cca88a
S637\adamo <votrubova.ada@gmail.com>
2022-02-20 09:26:23 +0100

    prosim kontrolu

--- main.js
+++ main.js
@@ -2,20 +2,39 @@
 
 console.log("ahoooj :-)");
 
-let nadpis = document.querySelector("h1");
+/** let nadpis = document.querySelector("h1");
 nadpis.classList.add("zluty");
-nadpis.classList.remove("zluty")
+
 
 let ctverecek = document.querySelector("div.zluty");
 ctverecek.classList.add("ctverecek");
+*/
 
-let odstavec =document.querySelector ("ul")
-odstavec.classList.add("red");
-odstavec.style.fontSize ="30px";
+let odstavec= document.querySelector("p")
+
+function ztucni(){
+    console log("test ztucni");
+    odstavec.style.fontWeight="bold";
+}
+ 
+function ztenci(){
+    console log("test ztenci")
+    odstavec.style.fontWeight="normal";
+}
+
+function prikliknuti(){
+    odstavec.classList.toggle("red");
+   }
+
+let pocitadlo= document.querySelector(".odstavec")
+function zvetsi(){
+    pocitadlo++;
+}
 
 
-
-
-
-
-**/ Domácí úkol 1: Tlačítko “červené po kliknutí” bude odstavci přidávat/odebírat třídu, která bude barvu písma v odstavci měnit na červenou.
+/**  Úkol na lekci: Vytvoř odstavec s textem “V elipse spí lev.”, který bude mít nastaveno style="font-size: 16px;", 
+ * a tlačítko s nápisem “červený po kliknutí”. Další požadavky na prvky stránky budou:
+Úkol na lekci: Písmo v odstavci po najetí myši ztuční.
+Domácí úkol 1: Tlačítko “červené po kliknutí” bude odstavci přidávat/odebírat třídu, která bude barvu písma v odstavci měnit na červenou.
+Domácí úkol 2: přidej tlačítko “větší po kliknutí”, které písmo zvětší o jeden pixel oproti jeho aktuální velikost.
+*/
